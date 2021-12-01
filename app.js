8dd2356cb2d3b69710a6d832fbf57f3793cf02a3
piyushpoudyal <72690317+piyushpoudyal@users.noreply.github.com>
2021-12-01 11:51:45 +0530

    style,featurecompleted website

--- app.js
+++ app.js
@@ -6,19 +6,21 @@ var output=document.querySelector("#outputB");
 
 
 function calculateProfitAndLoss(initial,quantity,current){
-    if(initial>current){
+if(initial>current){
 var loss=(initial-current)*quantity;
 var lossPercentage=(loss/initial)*100;
+output.style.color="red";
 showOutput(`Hey, the loss is ${loss} and the loss percentage is ${lossPercentage}% `);
 
     }else if(current>initial){
         var profit=(current - initial)*quantity;
         var profitPercentage=(profit/initial)*100;
         showOutput(`Hey, the profit is ${profit} and the profit percentage is ${profitPercentage}% `);
-        
+        output.style.color="green";
     }else{
 showOutput("No pain no gain,no gain no pain");
     }
+
 }
 function submitHandler(){
     var ip=  Number(initialPrice.value);
