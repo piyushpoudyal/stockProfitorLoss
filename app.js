920af1e37eb50f61e64a4a3f4aeae8c3e9de74b9
piyushpoudyal <72690317+piyushpoudyal@users.noreply.github.com>
2021-12-01 11:41:30 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var initialPrice=document.querySelector("#inputA");
+var stockBought=document.querySelector("#inputB");
+var currentPrice=document.querySelector("#inputC");
+var calculateButton=document.querySelector("#cal-btn");
+var output=document.querySelector("#outputB");
+
+
+function calculateProfitAndLoss(initial,quantity,current){
+    if(initial>current){
+var loss=(initial-current)*quantity;
+var lossPercentage=(loss/initial)*100;
+showOutput(`Hey, the loss is ${loss} and the loss percentage is ${lossPercentage}% `);
+
+    }else if(current>initial){
+        var profit=(current - initial)*quantity;
+        var profitPercentage=(profit/initial)*100;
+        showOutput(`Hey, the profit is ${profit} and the profit percentage is ${profitPercentage}% `);
+        
+    }else{
+showOutput("No pain no gain,no gain no pain");
+    }
+}
+function submitHandler(){
+    var ip=  Number(initialPrice.value);
+    var qty= Number(stockBought.value);
+    var curr=Number(currentPrice.value);
+    calculateProfitAndLoss(ip,qty,curr);
+
+}
+function showOutput(message,status){
+
+output.innerText=message;
+}
+
+calculateButton.addEventListener("click",submitHandler);
+
