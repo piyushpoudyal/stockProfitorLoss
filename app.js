bf12f3181564741e0b5a422872fb97b6a3c9674e
piyushpoudyal <72690317+piyushpoudyal@users.noreply.github.com>
2021-12-02 08:23:10 +0530

    fixed percentage bug

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ var output=document.querySelector("#outputB");
 function calculateProfitAndLoss(initial,quantity,current){
 if(initial>current){
 var loss=(initial-current)*quantity;
-var lossPercentage=(loss/initial)*100;
+const lossPercentage = loss/(initial*quantity) * 100;
 output.style.color="red";
 showOutput(`Hey, the loss is ${loss} and the loss percentage is ${lossPercentage}% `);
 
     }else if(current>initial){
         var profit=(current - initial)*quantity;
-        var profitPercentage=(profit/initial)*100;
+        const profitPercentage = profit/(initial*quantity) * 100;
         showOutput(`Hey, the profit is ${profit} and the profit percentage is ${profitPercentage}% `);
         output.style.color="green";
     }else{
@@ -26,7 +26,14 @@ function submitHandler(){
     var ip=  Number(initialPrice.value);
     var qty= Number(stockBought.value);
     var curr=Number(currentPrice.value);
-    calculateProfitAndLoss(ip,qty,curr);
+    // calculateProfitAndLoss(ip,qty,curr);
+    if(initialPrice.value == "" || stockBought.value == "" || currentPrice.value ==""){
+        showOutput("Input fields cannot",white)
+    }else if(initialPrice<1 || stockBought<1 || currentPrice<0){
+        showOutput("Value should be greater than zero",white)
+    } else{
+        calculateProfitAndLoss(ip,qty,curr);
+    }
 
 }
 function showOutput(message,status){
